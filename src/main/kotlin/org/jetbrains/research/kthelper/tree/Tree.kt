cd2a1922dd3a43cb6a79989e8ad1f0aa72470f1d
Azat Abdullin <abdullin@kspt.icc.spbstu.ru>
2022-02-02 19:25:26 +0300

    m

--- src/main/kotlin/org/jetbrains/research/kthelper/tree/Tree.kt
+++ src/main/kotlin/org/jetbrains/research/kthelper/tree/Tree.kt
@@ -1,11 +1,12 @@
 package org.jetbrains.research.kthelper.tree
 
-interface TreeNode {
-    val parent: TreeNode?
-    val children: Set<TreeNode>
-}
 
-interface Tree {
-    val root: TreeNode?
-    val nodes: Set<TreeNode>
+interface Tree<T : Tree.TreeNode<T>> {
+    val root: T?
+    val nodes: Set<T>
+
+    interface TreeNode<T> {
+        val parent: T?
+        val children: Set<T>
+    }
 }
