edec2846820394de07c7dca9d5fab0c8a89e9e2f
Azat Abdullin <azat.aam@gmail.com>
2024-03-13 17:39:23 +0100

    m

--- src/main/kotlin/org/vorpal/research/kthelper/files.kt
+++ src/main/kotlin/org/vorpal/research/kthelper/files.kt
@@ -5,7 +5,11 @@ package org.vorpal.research.kthelper
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.InputStream
+import java.nio.file.FileVisitResult
+import java.nio.file.Files
 import java.nio.file.Path
+import java.nio.file.SimpleFileVisitor
+import java.nio.file.attribute.BasicFileAttributes
 
 private const val MAX_BYTE_ARRAY_SIZE = 16384
 
@@ -31,3 +35,26 @@ fun Path.resolve(vararg names: String): Path {
     }
     return current
 }
+
+fun deleteOnExit(file: File) = deleteOnExit(file.toPath())
+fun deleteOnExit(directoryToBeDeleted: Path) = Runtime.getRuntime().addShutdownHook(Thread {
+    tryOrNull {
+        Files.walkFileTree(directoryToBeDeleted, object : SimpleFileVisitor<Path>() {
+            override fun visitFile(
+                file: Path,
+                @SuppressWarnings("unused") attrs: BasicFileAttributes
+            ): FileVisitResult {
+                file.toFile().deleteOnExit()
+                return FileVisitResult.CONTINUE
+            }
+
+            override fun preVisitDirectory(
+                dir: Path,
+                @SuppressWarnings("unused") attrs: BasicFileAttributes
+            ): FileVisitResult {
+                dir.toFile().deleteOnExit()
+                return FileVisitResult.CONTINUE
+            }
+        })
+    }
+})
